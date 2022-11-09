db854b4351adb6a262c6c1cbf6c4498bcc69c596
romainpfeiffer <romain@pfeiffer.fr>
2022-11-09 22:14:33 +0100

    ajout fonctionnalité transactions et gif

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -4,8 +4,11 @@ import {TransactionContext} from "../context/TransactionContext";
 
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
+import useFetch from '../hooks/useFetch';
 
 const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
+    const gifUrl = useFetch({keyword})
+
     return (
         <div classname="bg-[#181918] m-4 flex flex-1
         2x1:min-w-[450px]
@@ -29,15 +32,16 @@ const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, a
 
                         </>
                     )}
-
-
-                    <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-                        <p className="text-[#37c7da] font-bold">{timestamp}</p>
                     </div>
-
-                </div>
+                        <img 
+                            src={gifUrl || url}
+                            alt="gif"
+                            className="w-full h-64 2x:h-96 rounded-md shadow-lg object-cover"
+                        />
+                        <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+                            <p className="text-[#37c7da] font-bold">{timestamp}</p>
+                        </div>
             </div>
-
         </div>
     )
 }
